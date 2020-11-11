89ca58b6b943f1bc24920c2632f0f33b7b6087e1
ShazmaBatool <73420497+ShazmaBatool@users.noreply.github.com>
2020-11-11 13:16:21 +0500

    Add files via upload

---
+++ App.js
@@ -0,0 +1,28 @@
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import { StyleSheet, Text, View } from "react-native";
+import HomeScreen from "./app/HomeScreen";
+import Details from "./app/LoginScreen";
+import User from "./app/SignUpScreen";
+import AppNavigator from "./app/navigation/AppNavigator";
+
+const Stack = createStackNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <AppNavigator />
+    </NavigationContainer>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});

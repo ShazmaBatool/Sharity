514d9a642b2515c730c60d02a9de088413c6377e
ShazmaBatool <shazmabatool12edu@gmail.com>
2020-11-26 02:13:30 +0500

    DonationDetails ver1

---
+++ app/screens/screensOrg/DonationsDetails.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function DonationsDetails() {
+  return (
+    <div>
+      <Text>
+        {" "}
+        Donor Info Who Donated, Driver Info Who Delivered, Donation Type etc.
+      </Text>
+    </div>
+  );
+}

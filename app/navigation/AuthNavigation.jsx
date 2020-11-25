8d7656ae8720d40f58cae40aa3778cf62694dd8a
ShazmaBatool <shazmabatool12edu@gmail.com>
2020-11-26 02:19:31 +0500

    AuthNavigation ver1

--- app/navigation/AuthNavigation.jsx
+++ app/navigation/AuthNavigation.jsx
@@ -4,18 +4,18 @@ import { createStackNavigator } from "@react-navigation/stack";
 import OrgNavigation from "./OrgNavigation";
 import DonorNavigation from "./DonorNavigation";
 import DriverNavigation from "./DriverNavigation";
-import SignInScreen from "../screens/screensOrg/SignInScreen";
+import SignInOrg from "../screens/screensOrg/SignInOrg";
 import WelcomeScreen from "../screens/WelcomeScreen";
 
 export default function AuthNavigation() {
   const Stack = createStackNavigator();
   return (
     <Stack.Navigator screenOptions={{ headerShown: false }}>
-      <Stack.Screen name='Welcome' component={WelcomeScreen} />
-      <Stack.Screen name='SignIn' component={SignInScreen} />
-      <Stack.Screen name='Org' component={OrgNavigation} />
-      <Stack.Screen name='Donor' component={DonorNavigation} />
-      <Stack.Screen name='Driver' component={DriverNavigation} />
+      <Stack.Screen name="Welcome" component={WelcomeScreen} />
+      <Stack.Screen name="SignIn" component={SignInOrg} />
+      <Stack.Screen name="Org" component={OrgNavigation} />
+      <Stack.Screen name="Donor" component={DonorNavigation} />
+      <Stack.Screen name="Driver" component={DriverNavigation} />
     </Stack.Navigator>
   );
 }

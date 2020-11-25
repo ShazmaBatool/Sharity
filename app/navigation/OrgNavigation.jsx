c4bfcb10f684d3887bd9b85300053531641fe0fe
ShazmaBatool <shazmabatool12edu@gmail.com>
2020-11-26 02:18:56 +0500

    OrgNavigation ver1

--- app/navigation/OrgNavigation.jsx
+++ app/navigation/OrgNavigation.jsx
@@ -1,15 +1,17 @@
 import React from "react";
 import { createStackNavigator } from "@react-navigation/stack";
 
-import SignInScreen from "../screens/screensOrg/SignInScreen";
+import SignInOrg from "../screens/screensOrg/SignInOrg";
 import WelcomeOrgScreen from "../screens/screensOrg/WelcomeOrgScreen";
+import OrgHomeScreen from "../screens/screensOrg/OrgHomeScreen";
 
 export default function OrgNavigation() {
   const Stack = createStackNavigator();
   return (
     <Stack.Navigator screenOptions={{ headerShown: false }}>
-      <Stack.Screen name='WelcomeOrg' component={WelcomeOrgScreen} />
-      <Stack.Screen name='SignIn' component={SignInScreen} />
+      <Stack.Screen name="WelcomeOrg" component={WelcomeOrgScreen} />
+      <Stack.Screen name="SignIn" component={SignInOrg} />
+      <Stack.Screen name="OrgHomeScreen" component={OrgHomeScreen} />
     </Stack.Navigator>
   );
 }

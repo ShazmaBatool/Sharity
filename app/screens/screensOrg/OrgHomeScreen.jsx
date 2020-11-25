5b1fd31f64ec016d068533ad61315b79abdcb1a4
ShazmaBatool <shazmabatool12edu@gmail.com>
2020-11-26 02:12:15 +0500

    OrgHomeScreen ver1

---
+++ app/screens/screensOrg/OrgHomeScreen.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import { View, StyleSheet, TouchableOpacity, Text } from "react-native";
+import { Customization } from "../../config/Customization";
+import Header from "../../common/Header";
+
+export default function HomeScreen({ navigation }) {
+  return (
+    <View style={styles.container}>
+      <Text>Home Screen of organization</Text>
+    </View>
+  );
+}
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: "#fff",
+    paddingTop: 20,
+    alignItems: "center",
+    flex: 1,
+  },
+  detailsContainer: {
+    borderWidth: 1,
+    borderColor: "#FFC107",
+    height: 190,
+    marginTop: 20,
+    padding: 10,
+    width: 280,
+  },
+  detailsText: {
+    margin: 5,
+  },
+});
